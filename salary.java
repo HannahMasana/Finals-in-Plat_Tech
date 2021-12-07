fa79be4b5b059a9fa128f1cb92f15536a6d109f4
unknown <makathlyn.bugais@evsu.edu.ph>
2021-12-07 14:02:22 +0800

    Java Program (2nd Commit)

--- salary.java
+++ salary.java
@@ -14,20 +14,33 @@ class Main {
         double hrate = drate/8;
         System.out.printf("Your Hourly Rate: %.2f \n", hrate);
 
+        double tax_excess = 0;
         double atd = 0;
         if (salary <= 250000){
-            atd = 0;
+            tax_excess = salary - 0;
+            atd = (0 * tax_excess);
+
         }else if(salary >= 250000 && salary < 400000){
-            atd = 30000 * 0.2;
+            tax_excess = salary - 250000;
+            atd =  (0.2 * tax_excess);
+
         }else if(salary >= 400000 && salary < 800000){
-            atd = 30000 + (0.25 * (salary - 400000));
-        }else if(salary >= 800000 && salary < 200000000){
-            atd = 130000 + (0.3 * (salary - 800000));
-        }else if(salary >= 200000000 && salary < 800000000){
-            atd = 490000 + (0.32 * (salary - 200000000));
-        }else if(salary >= 800000000 ){
-            atd = 2410000 + (0.35 * (salary - 800000000));
+            tax_excess = (salary - 30000) - 400000;
+            atd = 30000 + (0.25 * tax_excess);
+
+        }else if(salary >= 800000 && salary < 2000000){
+            tax_excess = (salary - 130000) - 800000;
+            atd = 130000 + (0.3 * tax_excess);
+
+        }else if(salary >= 2000000 && salary < 8000000){
+            tax_excess = (salary - 490000) - 2000000;
+            atd = 490000 + (0.32 * tax_excess);
+
+        }else if(salary >= 8000000 ){
+            tax_excess = (salary - 2410000) - 8000000;
+            atd = 2410000 + (0.35 * tax_excess);
         }
+        
         System.out.printf("Your Annual Tax Deduction: %.2f \n", atd);
         
         double mtd = atd/12;
@@ -35,8 +48,6 @@ class Main {
     
         double netp = mrate - mtd;
         System.out.printf("Net Pay: %.2f \n", netp);
-        
-
         
     }
 }
